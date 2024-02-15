3d374a62a1fe9bfcec76c6321755e6b032debdb3
AndreyAko57 <andrey57@mail.ru>
2024-02-15 15:44:46 +0500

    [refactor] modify models file...

--- assets/models.py
+++ assets/models.py
@@ -17,6 +17,13 @@ class Position(models.Model):
 
 
 class Staff(models.Model):
+    def __str__(self):
+        return self.full_name
+
+    @property
+    def full_name(self):
+        return f"{self.last_name} {self.first_name} {self.patronymic}"
+
     last_name = models.CharField(max_length=100)
     first_name = models.CharField(max_length=100)
     patronymic = models.CharField(max_length=100)
