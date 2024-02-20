19f7048d0de21fd10ed7eb039871e35e9f65dd5f
AndreyAko57 <andrey57@mail.ru>
2024-02-20 15:40:00 +0500

    [refactor] modify files...

--- assets/views.py
+++ assets/views.py
@@ -5,5 +5,4 @@ from assets.tasks import AssetRecalculator
 def recalculate_assets_view(request):
     recalculator = AssetRecalculator()
     recalculator.recalculate_assets()
-    # Дополнительный код, который выполняется после перерасчета активов
     return HttpResponse('Assets recalculation is complete')  # Пример возврата HTTP-ответа
