0db1a8c6c473ec937caaf6ed12f5a34666d89f58
AndreyAko57 <andrey57@mail.ru>
2024-02-20 12:54:33 +0500

    [refactor] modify files...

--- eAssets/urls.py
+++ eAssets/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from assets.api.views import create_user
+from assets.tasks import recalculate_assets
 
 urlpatterns = [
     path('', include('index.urls')),
@@ -29,4 +30,6 @@ urlpatterns = [
     path('auth/sign-in/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/token/verify/', TokenVerifyView.as_view(), name='verify_refresh'),
+
+    path('recalculation/', recalculate_assets, name='recalculate_assets'),
 ]
