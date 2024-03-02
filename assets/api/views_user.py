732be73e1d0702e14491a00b18d7518c1c132f7a
AndreyBorisovichKoval <andrey57@mail.ru>
2024-03-02 16:05:28 +0500

    [refactor] Modify all files...

--- assets/api/views_user.py
+++ assets/api/views_user.py
@@ -14,19 +14,57 @@ from assets.api.serializers import UserSerializer, UserSettingsSerializer
 logger = logging.getLogger('application')
 
 
-@api_view(["GET"])
+# @api_view(["GET"])
+# @authentication_classes([JWTAuthentication])
+# @permission_classes([IsAuthenticated])
+# def get_user_settings(request):
+#     user = request.user
+#     try:
+#         settings = UserSettings.objects.get(user=user)
+#         serializer = UserSettingsSerializer(settings)
+#         logger.info(f"User settings fetched for user: {user.username}")
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#     except UserSettings.DoesNotExist:
+#         logger.error(f"User settings not found for user: {user.username}")
+#         return Response({'error': 'Settings not found'}, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(["GET", "POST", "PATCH"])
 @authentication_classes([JWTAuthentication])
 @permission_classes([IsAuthenticated])
-def get_user_settings(request):
+def user_settings(request):
     user = request.user
     try:
         settings = UserSettings.objects.get(user=user)
-        serializer = UserSettingsSerializer(settings)
-        logger.info(f"User settings fetched for user: {user.username}")
-        return Response(serializer.data, status=status.HTTP_200_OK)
+
+        if request.method == "GET":
+            serializer = UserSettingsSerializer(settings)
+            logger.info(f"Настройки пользователя получены для пользователя: {user.username}")
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        elif request.method == "POST":
+            serializer = UserSettingsSerializer(settings, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                logger.info(f"Настройки пользователя добавлены для пользователя: {user.username}")
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                logger.error(f"Недопустимые данные для обновления настроек пользователя для пользователя: {user.username}")
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        elif request.method == "PATCH":
+            serializer = UserSettingsSerializer(settings, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                logger.info(f"Настройки пользователя обновлены для пользователя: {user.username}")
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                logger.error(f"Недопустимые данные для обновления настроек пользователя для пользователя: {user.username}")
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     except UserSettings.DoesNotExist:
-        logger.error(f"User settings not found for user: {user.username}")
-        return Response({'error': 'Settings not found'}, status=status.HTTP_404_NOT_FOUND)
+        logger.error(f"Настройки пользователя не найдены для пользователя: {user.username}")
+        return Response({'error': 'Настройки не найдены'}, status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(["POST"])
--- eAssets/urls.py
+++ eAssets/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
-from assets.api.views_user import view_users, create_user, change_password, get_user_settings
+from assets.api.views_user import view_users, create_user, change_password, user_settings
 from assets.views import recalculate_assets_view
 from django.contrib.auth import views as auth_views
 
@@ -54,7 +54,7 @@ urlpatterns = [
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/token/verify/', TokenVerifyView.as_view(), name='verify_refresh'),
 
-    path('auth/user_settings/', get_user_settings, name='get_user_settings'),
+    path('auth/user_settings/', user_settings, name='get_user_settings'),
 
 
     path('recalculation/', recalculate_assets_view, name='recalculate_assets'),
