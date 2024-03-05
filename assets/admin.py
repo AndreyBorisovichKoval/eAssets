a521ebe0b3a009e62ee8a721c8eabac8ebdc9493
AndreyAko57 <andrey57@mail.ru>
2024-03-05 08:18:28 +0500

    [refactor] Modify All Files...

--- assets/admin.py
+++ assets/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from assets.models import Department, Division, Position, Staff, Asset, AssetType, AssetAssignment, UserAction
+from assets.models import Department, Division, Position, Staff, Asset, AssetType, AssetAssignment, TaskCheckPoint, UserAction, UserSettings
+
 
 admin.site.register(Department)
 admin.site.register(Division)
@@ -8,4 +9,6 @@ admin.site.register(Staff)
 admin.site.register(Asset)
 admin.site.register(AssetType)
 admin.site.register(AssetAssignment)
+admin.site.register(TaskCheckPoint)
 admin.site.register(UserAction)
+admin.site.register(UserSettings)
--- assets/api/views_tasks.py
+++ assets/api/views_tasks.py
@@ -1,8 +1,12 @@
+import logging
 from datetime import datetime
 from decimal import Decimal
 # from django.db.models import F
 from assets.models import Asset, TaskCheckPoint
 # from additional.email_sender import send_email
+
+
+logger = logging.getLogger('application')
 
 
 class AssetRecalculator:
@@ -41,43 +45,10 @@ class AssetRecalculator:
                 task_check_point.is_successful = True
                 task_check_point.save()
 
+            logger.info(f"Successful completion of recalculation of fixed assets...")
+
         except TaskCheckPoint.DoesNotExist:
             return TaskCheckPoint.DoesNotExist
-
-
-# def task_recalculate_assets():
-#     try:
-#         task_check_point = TaskCheckPoint.objects.get(title='Assets Recalculation')
-#         last_processed_date = task_check_point.last_processed_date
-#         is_successful = task_check_point.is_successful
-#
-#         current_date = datetime.now().date()  # Сохраняем текущую дату в переменную
-#
-#         if (last_processed_date.month != current_date.month or
-#             last_processed_date.year != current_date.year or
-#             not is_successful):
-#             task_check_point.is_successful = False
-#             task_check_point.save()
-#
-#             assets = Asset.objects.filter(is_written_off=False)
-#
-#             for asset in assets:
-#                 acquisition_date = asset.acquisition_date
-#                 months_passed = (current_date.year - acquisition_date.year) * 12 + (
-#                     current_date.month - acquisition_date.month)
-#                 depreciation_per_month = asset.cost / (asset.service_life * 12)
-#                 remaining_cost = asset.cost - (depreciation_per_month * months_passed)
-#                 asset.current_cost = Decimal(remaining_cost)
-#                 asset.last_recalculation_date = current_date  # Используем сохраненную текущую дату
-#                 asset.save()
-#
-#             task_check_point.last_processed_date = current_date
-#             task_check_point.is_successful = True
-#             task_check_point.save()
-#
-#     except TaskCheckPoint.DoesNotExist:
-#         return TaskCheckPoint.DoesNotExist
-
 
 
 # def recalculate_assets():
@@ -123,4 +94,3 @@ class AssetRecalculator:
 #     except TaskCheckPoint.DoesNotExist:
 #         # Запись TaskCheckPoint не найдена
 #         return TaskCheckPoint.DoesNotExist
-
