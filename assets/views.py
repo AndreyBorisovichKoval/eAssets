b766537825701a5e0ad70a8d3fc2313f8c0d0021
AndreyBorisovichKoval <andrey57@mail.ru>
2024-03-07 10:49:10 +0500

    [refactor] Modify All files...

--- assets/views.py
+++ assets/views.py
@@ -2,11 +2,6 @@ from django.http import HttpResponse, JsonResponse
 from assets.api.views_tasks import AssetRecalculator
 
 
-# def recalculate_assets_view(request):
-#     recalculator = AssetRecalculator()
-#     recalculator.recalculate_assets()
-#     return HttpResponse('Assets recalculation is complete')
-
 def recalculate_assets_view(request):
     recalculator = AssetRecalculator()
     recalculator.recalculate_assets()
